262ff9e648d4e6533e1c2828d05c9e7a6f17044c
Juan Veras <juan@vvssistemas.com.br>
2025-10-01 00:48:08 -0300

    ajustes schema pydantic de postagem agendada

--- interfaces/api/schemas/postagem_agendada_schemas.py
+++ interfaces/api/schemas/postagem_agendada_schemas.py
@@ -1,27 +1,38 @@
-
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
+from domain.entities.postagens_agendadas.postagem_agendada import StatusDaPostagem
+from domain.entities.videos.video import Video
+from domain.entities.contas.conta import Conta
 
 
 class PostagemAgendadaCreateRequest(BaseModel):
-    """Schema para criação de vídeo"""
+    """Schema para criação de postagem agendada"""
     descricao: str
     data_para_envio: datetime
     id_video: str
+    contas: Optional[List[Conta]] = []
+
 
 class PostagemAgendadaUpdateRequest(BaseModel):
-    """Schema para atualização de vídeo"""
-    descricao: str
+    """Schema para atualização de postagem agendada"""
+    descricao: Optional[str] = None
     data_para_envio: Optional[datetime] = None
-    id_video: str
+    id_video: Optional[str] = None
+    contas: Optional[List[Conta]] = None
+    status: Optional[StatusDaPostagem] = None
+
 
 class PostagemAgendadaResponse(BaseModel):
-    """Schema para resposta de vídeo"""
+    """Schema para resposta de postagem agendada"""
     id: str
     descricao: str
     data_para_envio: datetime
+    id_video: str
+    video: Video
+    contas: List[Conta]
+    status: StatusDaPostagem
 
-model_config = {
+    model_config = {
         "from_attributes": True  # Pydantic v2
     }
